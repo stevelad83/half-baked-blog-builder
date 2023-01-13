83ba450afd2d41fcb5beeb1cd5dfd252e2b1bd0b
Steven Bryant <stevelad_83@yahoo.com>
2023-01-13 18:24:46 -0500

    refactored event handler funcs

--- src/components/Editor/Editor.js
+++ src/components/Editor/Editor.js
@@ -13,34 +13,52 @@ export default function Editor({
   text,
   setText,
 }) {
-  const handleTitle = (e) => {
-    setTitle(e.target.value);
+  const handleChange = (value, e) => {
+    if (value === 'title') {
+      setTitle(e.target.value);
+    }
+    if (value === 'subtitle') {
+      setSubtitle(e.target.value);
+    }
+    if (value === 'font') {
+      setFont(e.target.value);
+    }
+    if (value === 'align') {
+      setAlign(e.target.value);
+    }
+    if (value === 'text') {
+      setText(e.target.value);
+    }
   };
-  const handleSubtitle = (e) => {
-    setSubtitle(e.target.value);
-  };
-  const handleFont = (e) => {
-    setFont(e.target.value);
-  };
-  const handleAlign = (e) => {
-    setAlign(e.target.value);
-  };
-  const handleText = (e) => {
-    setText(e.target.value);
-  };
+
+  // const handleTitle = (e) => {
+  //   setTitle(e.target.value);
+  // };
+  // const handleSubtitle = (e) => {
+  //   setSubtitle(e.target.value);
+  // };
+  // const handleFont = (e) => {
+  //   setFont(e.target.value);
+  // };
+  // const handleAlign = (e) => {
+  //   setAlign(e.target.value);
+  // };
+  // const handleText = (e) => {
+  //   setText(e.target.value);
+  // };
 
   return (
     <div className="editor">
       <div className="form-control">
-        <input name="title" type="text" value={title} onChange={handleTitle} />
+        <input name="title" type="text" value={title} onChange={(e) => handleChange('title', e)} />
         <label htmlFor="title">Title</label>
       </div>
       <div className="form-control">
-        <input type="text" value={subtitle} onChange={handleSubtitle} />
+        <input type="text" value={subtitle} onChange={(e) => handleChange('subtitle', e)} />
         <label>Subtitle</label>
       </div>
       <div className="form-control">
-        <select value={font} onChange={handleFont}>
+        <select value={font} onChange={(e) => handleChange('font', e)}>
           <option value="architect">{"Architect's Daughter"}</option>
           <option value="comforter">Comforter</option>
           <option value="fredoka">Fredoka</option>
@@ -54,23 +72,45 @@ export default function Editor({
       </div>
       <div className="form-control">
         <label>Alignment</label>
-        <div className="radio-group" onChange={handleAlign}>
+        <div className="radio-group" onChange={(e) => handleChange('align', e)}>
           <label>
-            <input name="align" type="radio" value="left" checked={align === 'left'} />
+            <input
+              name="align"
+              type="radio"
+              value="left"
+              checked={align === 'left'}
+              onChange={(e) => handleChange('align', e)}
+            />
             <i className="ri-align-left"></i>
           </label>
           <label>
-            <input name="align" type="radio" value="center" checked={align === 'center'} />
+            <input
+              name="align"
+              type="radio"
+              value="center"
+              checked={align === 'center'}
+              onChange={(e) => handleChange('align', e)}
+            />
             <i className="ri-align-center"></i>
           </label>
           <label>
-            <input name="align" type="radio" value="right" checked={align === 'right'} />
+            <input
+              name="align"
+              type="radio"
+              value="right"
+              checked={align === 'right'}
+              onChange={(e) => handleChange('align', e)}
+            />
             <i className="ri-align-right"></i>
           </label>
         </div>
       </div>
       <div className="form-control">
-        <textarea value={text} onChange={handleText} style={{ height: '250px' }} />
+        <textarea
+          value={text}
+          onChange={(e) => handleChange('text', e)}
+          style={{ height: '250px' }}
+        />
         <label>Text</label>
       </div>
     </div>
